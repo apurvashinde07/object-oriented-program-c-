85da36a2cacaef816ebb2b27031175b1c1553d20
Apurva shinde <apurvashinde077@gmail.com>
2025-09-27 23:09:18 +0530

    Add files via upload

---
+++ opertors.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int a= 5;
+    cout<<a<<endl;
+    cout<<"hello";
+}
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout<<"enter the value of num1"; /*it is inserting operator*/
+    cin>>num1; /*is a extraction opertor*/
+
+    cout<<"enter the value of num2"; /*it is inserting operator*/
+    cin>>num2; /*is a extraction opertor*/
+
+    cout<<"the sum is"<< num1+num2;
+    
+}
+int main(){
+    int a=4, b=5;
+    cout<<"opertors in c++"<<endl;
+    cout<<"arithmetic opertors";
+    cout<<"the value od a+b is:"<<a+b<<endl;
+    cout<<"the value of a%b is:"<<a%b<<endl;
+    cout<<"the value of a-b is:"<<a-b<<endl;
+    cout<<"the value of a*b is:"<<a*b<<endl;
+    cout<<"the value of a/b is:"<<a/b<<endl;
+    cout<<"the value of a++ is:"<<a++<<endl;
+    cout<<"the value of a-- is:"<<a--<<endl;
+    cout<<"the value of ++a is:"<<++a<<endl;
+    cout<<"the value of --a is:"<<--a<<endl;
+    }
+
+int main(){
+    int a=4, b=5;
+    cout<<"comparison opertors"<<endl;
+    cout<<"the value of a==b is:"<<(a==b)<<endl;
+    cout<<"the value of a!=b is:"<<(a!=b)<<endl;
+    cout<<"the value of a<b is:"<<(a<b)<<endl;
+    cout<<"the value of a<b is:"<<(a<b)<<endl;
+    cout<<"the value of a>b is:"<<(a>b)<<endl;
+    cout<<"the value of a<=b is:"<<(a<=b)<<endl;
+    cout<<"the value of a>=b is:"<<(a>=b)<<endl;  
+
+    //logical opertors
+    cout<<"the value of (a==b) && (a<b) is"<<((a==b) && (a<b))<<endl;
+    cout<<"The value of this logical or operator ((a==b) || (a<b)) is:"<<((a==b) || (a<b))<<endl; 
+    cout<<"The value of this logical not operator (!(a==b)) is:"<<(!(a==b))<<endl; 
+
+
+    return 0;
+}
