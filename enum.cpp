cc41be9be9276b92893d1e43a2db2252f7754d7e
Apurva shinde <apurvashinde077@gmail.com>
2025-10-06 23:13:48 +0530

    Add files via upload

---
+++ enum.cpp
@@ -0,0 +1,13 @@
+//...enum...
+#include<iostream>
+using namespace std;
+int main(){
+    enum Meal{ breakfast, lunch, dinner};
+    Meal m1 = breakfast;
+    Meal m2 = lunch;
+    Meal m3 = dinner;
+    cout<<m1<<endl;
+    cout<<m2;
+    return 0;
+}
+    
